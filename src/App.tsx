16efae177dc8357b859c46b2f4c835584e811972
Arnav Patel <patelarnavm+github@gmail.com>
2023-02-09 10:15:59 -0500

    made constants and useInterval and boiler plate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,57 @@
-import React from "react";
-import Board from "./components/Board";
-import Food from "./components/Food";
-import Snake from "./components/Snake";
+import React, { useState, useEffect, useRef } from "react";
+import useInterval from "./useInterval";
+import {
+  SIZE,
+  SCALE,
+  SPEED,
+  DIRECTIONS,
+  START_SNAKE,
+  START_FOOD,
+} from "./constants";
 import "./App.css";
 
 function App() {
+  const board = useRef<HTMLDivElement>(null!);
+  const [snake, setSnake] = useState(START_SNAKE);
+  const [food, setFood] = useState(START_FOOD);
+  const [gameOver, setGameOver] = useState(false);
+  const [direction, setDirection] = useState([0, -1]);
+  const [speed, setSpeed] = useState(null!);
+
+  console.log(board);
+
+  if (board.current) {
+    board.current.style.height = `${SIZE[1]}px`;
+    board.current.style.width = `${SIZE[0]}px`;
+  }
+
+  function start() {}
+
+  function end() {}
+
+  function move(event: any) {}
+
+  function spawnFood() {}
+
+  function eatFood() {}
+
+  function crash() {}
+
+  function gameLoop() {}
+
+  useEffect(() => {}, [snake, food, gameOver]);
+
   return (
     <div>
-      <Board />
+      <div
+        role="button"
+        tabIndex={0}
+        className="board"
+        ref={board}
+        onKeyDown={(event) => move(event)}
+      ></div>
+      {gameOver && <div>Game Over!</div>}
+      <button onClick={start}>Start</button>
     </div>
   );
 }
