00dc4a24bec8fd14e396e577c272b75ebbd423d7
Arnav Patel <patelarnavm+github@gmail.com>
2023-02-10 11:03:39 -0500

    finished snake game, needs design

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,21 @@
-export const SIZE = [500, 500];
-export const SCALE: number = 20;
-export const TIME_DELAY: number = 100;
-export const DIRECTIONS = {
-  37: [-1, 0],
-  39: [1, 0],
-  38: [0 - 1],
-  40: [0, 1],
+interface direction {
+  LEFT: number[];
+  RIGHT: number[];
+  UP: number[];
+  DOWN: number[];
+}
+
+export const SIZE: number[] = [1000, 1000];
+export const SCALE: number = 50;
+export const START_SNAKE: number[][] = [
+  [4, 10],
+  [4, 10],
+];
+export const START_FOOD: number[] = [10, 10];
+export const DIRECTIONS: direction = {
+  LEFT: [-1, 0],
+  RIGHT: [1, 0],
+  UP: [0, -1],
+  DOWN: [0, 1],
 };
-export const INITIAL_SNAKE = [
-  [0, 1],
-  [0, 2],
-];
-export const INITIAL_FOOD = [
-  Math.floor((Math.random() * SIZE[0]) / SCALE),
-  Math.floor((Math.random() * SIZE[0]) / SCALE),
-];
+export const DELAY: number = 100;
